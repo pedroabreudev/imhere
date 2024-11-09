a3514fb1ac50eb0c6bdb2955db509d98e9950110
Pedro Henrique Abreu <pedroabreu.dev@gmail.com>
2024-11-09 17:16:02 -0400

    Removendo Participantes

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import {
   Text,
   View,
@@ -10,34 +11,28 @@ import { styles } from "./styles";
 import { Participant } from "@/src/components/Participant";
 
 export function Home() {
-  const participants = [
-    "Pedro",
-    "Henrique",
-    "Diego",
-    "Yuri",
-    "Juan",
-    "Jackson",
-    "Mateus",
-    "Lucas",
-    "Gabriel",
-    "André",
-    "William",
-  ];
+  const [participants, setParticipants] = useState<string[]>([]);
+  const [participantName, setParticipantName] = useState("");
+
   function handleParticipantAdd() {
-    if (participants.includes("Pedro")) {
+    if (participants.includes(participantName)) {
       return Alert.alert(
         "Participante existe",
         "Já existe um participante na lista com esse nome"
       );
     }
-    console.log("Você clicou no botão de Adicionar!");
+    setParticipants((prevState) => [...prevState, participantName]);
+    setParticipantName("");
   }
 
   function handleParticipantRemove(name: string) {
     Alert.alert("Remover", `Remover o participante ${name}?`, [
       {
         text: "Sim",
-        onPress: () => Alert.alert("Deletado!"),
+        onPress: () =>
+          setParticipants((prevState) =>
+            prevState.filter((participant) => participant !== name)
+          ),
       },
       {
         text: "Não",
@@ -56,6 +51,8 @@ export function Home() {
           style={styles.input}
           placeholder="Nome do participante"
           placeholderTextColor="#6B6B6B"
+          onChangeText={setParticipantName}
+          value={participantName}
         />
         <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
           <Text style={styles.buttonText}>+</Text>
@@ -66,6 +63,7 @@ export function Home() {
         keyExtractor={(item) => item}
         renderItem={({ item }) => (
           <Participant
+            key={item}
             name={item}
             onRemove={() => handleParticipantRemove(item)}
           />
